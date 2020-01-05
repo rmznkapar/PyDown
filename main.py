147234e2a1639128587d6e1dd4f5261df5c19d65
rmzn <rmznkapar@gmail.com>
2020-01-05 14:46:46 +0300

    class

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 import youtube_dl
-
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-
-result = ydl.extract_info(
-    'https://www.youtube.com/watch?v=4p_aQwIedN4',
-    download=False
-)
-
-if 'entries' in result:
-    video = result['entries'][0]
-else:
-    video = result
+from tkinter import *
 
 
-def download():
-    for format in video['formats']:
-        if format['acodec'] != 'none':
-            print(format['format_note'])
-            print(format['url'])
-            print(format)
-            print('\n******************************************************\n')
+class Downloader:
+    video = ''
+    tags = {}
+    urls = []
+
+    def __init__(self, user_link):
+        self.user_link = user_link
+
+    def list(self):
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+        result = ydl.extract_info(
+            user_link,
+            download=False
+        )
+        self.video = result
+    def tagger(self):
+        self.tags.update({
+            'id': self.video['id'],
+            'uploader': self.video['uploader'],
+            'title': self.video['title'],
+            'duration': self.video['duration'],
+            'thumbnail': self.video['thumbnail']
+        })
         pass
-    pass
+    def urler(self):
+        for url in self.video['formats'] :
+            if url['filesize'] != 'none':
+                self.urls.append({
+                    'format': url['format_note'],
+                    'ext': url['ext'],
+                    'filesize': str( url['filesize']*(10**-6) ),
+                    'url': url['url']
+                })
+                pass
+            pass
+        pass
 
-def tag():
-    print(video['id'])
-    print(video['uploader'])
-    print(video['title'])
-    print(video['duration'])
-    print(video['thumbnail'])
-    pass
 
-tag()
-download()
+
+
+
+
+window = Tk()
+
+window.title("PyDown")
+
+window.geometry('400x200')
+
+lbl = Label(window, text="YouTube Link")
+lbl.grid(column=0, row=0, padx=5, pady=5)
+txt = Entry(window,width=50)
+txt.grid(column=0, row=1, padx=5, pady=5)
+
+def clicked():
+    urlink = Downloader( txt.get() )
+    urlink.ydler()
+
+btn = Button(window, text="Click Me", command=clicked)
+btn.grid(column=1, row=1, pady=5)
+window.mainloop()
